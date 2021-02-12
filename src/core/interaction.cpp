0e7dc953c92e3725416f9afceea7d8f8eec98c1c
junnys6018 <junkanglim1@gmail.com>
2021-02-12 16:43:01 +0800

    SurfaceInteraction adjusts normals to point outside closed shapes

--- src/core/interaction.cpp
+++ src/core/interaction.cpp
@@ -1,16 +1,15 @@
-#include "pch.h"
 #include "interaction.h"
 #include "geometry/geometry.h"
+#include "pch.h"
 
 namespace yart
 {
-	SurfaceInteraction::SurfaceInteraction(const Vector3f& point, const Vector3f& ptError,
-										   const Vector2f& uv, const Vector3f& wo,
-										   const Vector3f& dpdu, const Vector3f& dpdv,
+	SurfaceInteraction::SurfaceInteraction(const Vector3f& point, const Vector3f& ptError, const Vector2f& uv,
+										   const Vector3f& wo, const Vector3f& dpdu, const Vector3f& dpdv,
 										   const Vector3f& dndu, const Vector3f& dndv, Float time,
 										   const AbstractGeometry* geometry)
-		: Interaction(point, Normalize(Cross(dpdu, dpdv)), ptError, wo, time), m_uv(uv),
-		  m_dpdu(dpdu), m_dpdv(dpdv), m_dndu(dndu), m_dndv(dndv), m_Geometry(geometry)
+		: Interaction(point, Normalize(Cross(dpdu, dpdv)), ptError, wo, time), m_uv(uv), m_dpdu(dpdu),
+		  m_dpdv(dpdv), m_dndu(dndu), m_dndv(dndv), m_Geometry(geometry)
 	{
 		m_Shading.m_Normal = m_Normal;
 		m_Shading.m_dpdu = dpdu;
@@ -18,16 +17,23 @@ namespace yart
 		m_Shading.m_dndu = dndu;
 		m_Shading.m_dndv = dndv;
 
-		// TODO: Adjust normal to ensure to normal points outwards in closed shapes
+		// Adjust normal to ensure to normal points outwards in closed shapes
+		if (m_Geometry && (m_Geometry->m_ReverseOrientation ^ m_Geometry->m_TransformSwapsHandedness))
+		{
+			m_Normal *= -1;
+			m_Shading.m_Normal *= -1;
+		}
 	}
-	void SurfaceInteraction::SetShadingGeometry(const Vector3f& dpdu, const Vector3f& dpdv,
-												const Vector3f& dndu, const Vector3f& dndv,
-												bool orientationIsAuthoritative)
+	void SurfaceInteraction::SetShadingGeometry(const Vector3f& dpdu, const Vector3f& dpdv, const Vector3f& dndu,
+												const Vector3f& dndv, bool orientationIsAuthoritative)
 	{
 		m_Shading.m_Normal = Normalize(Cross(dpdu, dpdv));
 
-		// TODO: Adjust normal to ensure to normal points outwards in closed shapes
-
+		// Adjust normal to ensure to normal points outwards in closed shapes
+		if (m_Geometry && (m_Geometry->m_ReverseOrientation ^ m_Geometry->m_TransformSwapsHandedness))
+		{
+			m_Normal *= -1;
+		}
 		// Ensure shading normal and surface normal lie in the same hemisphere
 		// bool orientationIsAuthoritative determines which normal to flip if they dont lie
 		// in the same hemisphere
