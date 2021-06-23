6c6144c1035403277ad8fc83920257d8c95db379
junnys6018 <junkanglim1@gmail.com>
2021-06-23 14:05:45 +0800

    Implemented BVH

--- YART-Lib/include/accelerators/bvh.h
+++ YART-Lib/include/accelerators/bvh.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "core/memory_util.h"
 #include "core/primitive.h"
 #include <vector>
 
@@ -6,19 +7,97 @@ namespace yart
 {
 	enum class SplitMethod
 	{
-		SAH,
-		HLBVH,
+		// SAH,
+		// HLBVH,
 		Middle,
-		EqualCounts,
+		// EqualCounts,
 		COUNT
 	};
 
 	extern const char* const SplitMethodNames[static_cast<int>(SplitMethod::COUNT)];
 
-	class BVHAccel : public AbstractAggregate
+	struct BVHBuildNode
+	{
+		void InitLeaf(int firstPrimOffset, int numPrimitives, const Bounds3f& bounds)
+		{
+			m_FirstPrimOffset = firstPrimOffset;
+			m_NumPrimitives = numPrimitives;
+			m_Bounds = bounds;
+			m_Children[0] = m_Children[1] = nullptr;
+		}
+
+		void InitInterior(int splitAxis, BVHBuildNode* c1, BVHBuildNode* c2)
+		{
+			m_Children[0] = c1;
+			m_Children[1] = c2;
+			m_Bounds = Union(c1->m_Bounds, c2->m_Bounds);
+			m_SplitAxis = splitAxis;
+			m_NumPrimitives = 0;
+		}
+
+		inline bool IsInteriorNode() const
+		{
+			return m_NumPrimitives == 0;
+		}
+
+		Bounds3f m_Bounds;
+		BVHBuildNode* m_Children[2];
+		int m_SplitAxis, m_FirstPrimOffset, m_NumPrimitives;
+	};
+
+	struct BVHLinearNode
+	{
+		void InitFromBuildNode(const BVHBuildNode& buildNode)
+		{
+			m_Bounds = buildNode.m_Bounds;
+			m_NumPrimitives = buildNode.m_NumPrimitives;
+			m_SplitAxis = buildNode.m_SplitAxis;
+
+			if (!buildNode.IsInteriorNode())
+			{
+				m_FirstPrimOffset = buildNode.m_FirstPrimOffset;
+			}
+		}
+
+		inline bool IsInteriorNode() const
+		{
+			return m_NumPrimitives == 0;
+		}
+
+		Bounds3f m_Bounds;
+		union
+		{
+			int m_FirstPrimOffset;	 // Leaf node
+			int m_SecondChildOffset; // Interior node
+		};
+		uint16_t m_NumPrimitives; // 0: Interior node
+		uint8_t m_SplitAxis;
+	};
+
+	struct BVHPrimitiveInfo
+	{
+		BVHPrimitiveInfo() = default;
+		BVHPrimitiveInfo(size_t primitiveNumber, const Bounds3f& bounds)
+			: m_PrimitiveNumber(primitiveNumber), m_Bounds(bounds), m_Center((real)0.5 * (bounds.m_MinBound + bounds.m_MaxBound))
+		{
+		}
+
+		size_t m_PrimitiveNumber;
+		Bounds3f m_Bounds;
+		Vector3f m_Center;
+	};
+
+	class BVHAccelerator : public AbstractAggregate
 	{
 	public:
-		BVHAccel(const std::vector<Ref<AbstractPrimitive>>& primitives, int maxPrimsInNode, SplitMethod splitMethod);
+		BVHAccelerator(const std::vector<Ref<AbstractPrimitive>>& primitives, int maxPrimsInNode, SplitMethod splitMethod);
+		~BVHAccelerator()
+		{
+			if (m_BVHTree)
+			{
+				FreeAligned(m_BVHTree);
+			}
+		}
 
 		virtual Bounds3f WorldBound() const override;
 		virtual bool IntersectRay(const Ray& ray, SurfaceInteraction* surfaceInt) const override;
@@ -28,5 +107,12 @@ namespace yart
 		const int m_MaxPrimsInNode;
 		const SplitMethod m_SplitMethod;
 		std::vector<Ref<AbstractPrimitive>> m_Primitives;
+		BVHLinearNode* m_BVHTree = nullptr;
+
+	private:
+		BVHBuildNode* RecursiveBuild(MemoryArena& arena, std::vector<BVHPrimitiveInfo>& primitiveInfo, int start, int end,
+									 int* totalNodes, std::vector<Ref<AbstractPrimitive>>& orderedPrimitives);
+		BVHLinearNode* FlattenBVHTree(BVHBuildNode* root, int totalNodes);
 	};
+
 }
--- YART-Lib/src/accelerators/bvh.cpp
+++ YART-Lib/src/accelerators/bvh.cpp
@@ -1,34 +1,16 @@
 #include "pch.h"
+
 #include "accelerators/bvh.h"
 
 namespace yart
 {
-	const char* const SplitMethodNames[] = {"SAH", "HLBVH", "Middle", "EqualCounts"};
+	// const char* const SplitMethodNames[] = {"SAH", "HLBVH", "Middle", "EqualCounts"};
+	const char* const SplitMethodNames[] = {"Middle"};
 
-	struct BVHPrimitiveInfo
-	{
-		BVHPrimitiveInfo() = default;
-		BVHPrimitiveInfo(size_t primitiveNumber, const Bounds3f& bounds)
-			: m_PrimitiveNumber(primitiveNumber), m_Bounds(bounds), m_Center((real)0.5 * (bounds.m_MinBound + bounds.m_MaxBound))
-		{
-
-		}
-
-		size_t m_PrimitiveNumber;
-		Bounds3f m_Bounds;
-		Vector3f m_Center;
-	};
-
-	BVHAccel::BVHAccel(const std::vector<Ref<AbstractPrimitive>>& primitives, int maxPrimsInNode,
-					   SplitMethod splitMethod)
+	BVHAccelerator::BVHAccelerator(const std::vector<Ref<AbstractPrimitive>>& primitives, int maxPrimsInNode,
+								   SplitMethod splitMethod)
 		: m_MaxPrimsInNode(std::min(255, maxPrimsInNode)), m_SplitMethod(splitMethod), m_Primitives(primitives)
 	{
-		if (splitMethod != SplitMethod::SAH)
-		{
-			LOG_ERROR("BVHAccel::BVHAccel() SplitMethod = {} not yet implemented", SplitMethodNames[static_cast<int>(splitMethod)]);
-			return;
-		}
-
 		if (primitives.size() == 0)
 		{
 			return;
@@ -39,20 +21,127 @@ namespace yart
 		{
 			primitiveInfo[i] = {i, primitives[i]->WorldBound()};
 		}
+
+		MemoryArena arena(1024 * 1024);
+		int totalNodes = 0;
+		std::vector<Ref<AbstractPrimitive>> orderedPrimitives;
+		BVHBuildNode* root = RecursiveBuild(arena, primitiveInfo, 0, primitives.size(), &totalNodes, orderedPrimitives);
+		m_BVHTree = FlattenBVHTree(root, totalNodes);
 	}
 
-	Bounds3f BVHAccel::WorldBound() const
+	Bounds3f BVHAccelerator::WorldBound() const
 	{
-		return Bounds3f{};
+		if (m_BVHTree)
+		{
+			return m_BVHTree[0].m_Bounds;
+		}
 	}
 
-	bool BVHAccel::IntersectRay(const Ray& ray, SurfaceInteraction* surfaceInt) const
+	bool BVHAccelerator::IntersectRay(const Ray& ray, SurfaceInteraction* surfaceInt) const
 	{
 		return false;
 	}
 
-	bool BVHAccel::IntersectRay(const Ray& ray) const
+	bool BVHAccelerator::IntersectRay(const Ray& ray) const
 	{
 		return false;
+	}
+
+	BVHBuildNode* BVHAccelerator::RecursiveBuild(MemoryArena& arena, std::vector<BVHPrimitiveInfo>& primitiveInfo, int start,
+												 int end, int* totalNodes, std::vector<Ref<AbstractPrimitive>>& orderedPrimitives)
+	{
+		BVHBuildNode* node = arena.Alloc<BVHBuildNode>();
+		(*totalNodes)++;
+
+		Bounds3f totalBound;
+		for (int i = start; i < end; i++)
+		{
+			totalBound = Union(totalBound, primitiveInfo[i].m_Bounds);
+		}
+
+		int numPrimitives = end - start;
+		if (numPrimitives == 1)
+		{
+			int firstPrimOffset = orderedPrimitives.size();
+			int primitiveNumber = primitiveInfo[start].m_PrimitiveNumber;
+			orderedPrimitives.push_back(m_Primitives[primitiveNumber]);
+			node->InitLeaf(firstPrimOffset, 1, totalBound);
+		}
+		else
+		{
+			Bounds3f centroidBounds;
+			for (int i = start; i < end; i++)
+			{
+				centroidBounds = Union(centroidBounds, primitiveInfo[i].m_Center);
+			}
+
+			int axis = centroidBounds.MaximumExtent();
+			int mid = (start + end) / 2;
+			switch (m_SplitMethod)
+			{
+			case SplitMethod::Middle:
+				real midPoint = (centroidBounds.m_MinBound[axis] + centroidBounds.m_MaxBound[axis]) / 2;
+				BVHPrimitiveInfo* begin = primitiveInfo.data() + start;
+				// clang-format off
+				BVHPrimitiveInfo* midPrimitive = std::partition(begin, primitiveInfo.data() + end,
+					[midPoint, axis](const BVHPrimitiveInfo& pInfo) {
+						return pInfo.m_Center[axis] < midPoint;
+					});
+				// clang-format on
+				mid = midPrimitive - begin;
+				break;
+			}
+
+			node->InitInterior(axis, RecursiveBuild(arena, primitiveInfo, start, mid, totalNodes, orderedPrimitives),
+							   RecursiveBuild(arena, primitiveInfo, mid, end, totalNodes, orderedPrimitives));
+		}
+		return node;
+	}
+
+	BVHLinearNode* BVHAccelerator::FlattenBVHTree(BVHBuildNode* root, int totalNodes)
+	{
+		BVHLinearNode* BVHTree = AllocAligned<BVHLinearNode>(totalNodes);
+		int offset = 0;
+
+		struct TaggedBuildNode
+		{
+			int parentOffset;
+			BVHBuildNode* node;
+
+			BVHBuildNode* operator->()
+			{
+				return node;
+			}
+
+			BVHBuildNode operator*()
+			{
+				return *node;
+			}
+		};
+
+		std::vector<TaggedBuildNode> unvistedNodes;
+
+		unvistedNodes.push_back({-1, root});
+
+		while (unvistedNodes.size() != 0)
+		{
+			TaggedBuildNode node = unvistedNodes.back();
+			unvistedNodes.pop_back();
+
+			if (node.parentOffset != -1)
+			{
+				BVHTree[node.parentOffset].m_SecondChildOffset = offset;
+			}
+			BVHTree[offset].InitFromBuildNode(*node);
+			offset++;
+
+			if (node->IsInteriorNode())
+			{
+				unvistedNodes.push_back({offset, node->m_Children[0]});
+				unvistedNodes.push_back({-1, node->m_Children[1]});
+			}
+		}
+
+		return BVHTree;
 	}
 }
