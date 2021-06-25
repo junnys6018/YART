5a99fbeb1fdcb89c3c56979748aa9c6087fa6b94
junnys6018 <junkanglim1@gmail.com>
2021-06-25 16:55:45 +0800

    Spectrum XYZ color

--- YART-Lib/include/core/spectrum.h
+++ YART-Lib/include/core/spectrum.h
@@ -109,9 +109,35 @@ namespace yart
 	void SortSpectrumSamples(real* lambda, real* values, int n);
 	real AverageSpectrumSamples(const real* lambda, const real* values, int n, real lambdaStart, real lambdaEnd);
 
-	static constexpr const int s_SampledLambdaStart = 400;
-	static constexpr const int s_SampledLambdaEnd = 700;
-	static constexpr const int s_NumSpectralSamples = 60;
+	static constexpr int s_SampledLambdaStart = 400;
+	static constexpr int s_SampledLambdaEnd = 700;
+	static constexpr int s_NumSpectralSamples = 60;
+
+	// CIE X, Y, Z look up tables
+	static constexpr int s_NumCIESamples = 471;
+	extern const real CIE_X[s_NumCIESamples];
+	extern const real CIE_Y[s_NumCIESamples];
+	extern const real CIE_Z[s_NumCIESamples];
+	extern const real CIE_LAMBDA[s_NumCIESamples];
+	static constexpr real s_CIELuminanceFactor = 106.856895;
+
+	inline std::array<real, 3> XYZToRGB(const std::array<real, 3>& xyz)
+	{
+		std::array<real, 3> rgb;
+		rgb[0] = 3.240479f * xyz[0] - 1.537150f * xyz[1] - 0.498535f * xyz[2];
+		rgb[1] = -0.969256f * xyz[0] + 1.875991f * xyz[1] + 0.041556f * xyz[2];
+		rgb[2] = 0.055648f * xyz[0] - 0.204043f * xyz[1] + 1.057311f * xyz[2];
+		return rgb;
+	}
+
+	inline std::array<real, 3> RGBToXYZ(const std::array<real, 3>& rgb)
+	{
+		std::array<real, 3> xyz;
+		xyz[0] = 0.412453f * rgb[0] + 0.357580f * rgb[1] + 0.180423f * rgb[2];
+		xyz[1] = 0.212671f * rgb[0] + 0.715160f * rgb[1] + 0.072169f * rgb[2];
+		xyz[2] = 0.019334f * rgb[0] + 0.119193f * rgb[1] + 0.950227f * rgb[2];
+		return xyz;
+	}
 
 	// Samples are evenly spaced in the range [400, 700]
 	class SampledSpectrum : public BasisSpectrum<s_NumSpectralSamples>
@@ -119,6 +145,46 @@ namespace yart
 	public:
 		SampledSpectrum(real v = 0) : BasisSpectrum(v)
 		{
+		}
+
+		std::array<real, 3> ToXYZ() const
+		{
+			std::array<real, 3> xyz{0, 0, 0};
+			for (int i = 0; i < s_NumSpectralSamples; i++)
+			{
+				xyz[0] += X.m_Coefficients[i] * m_Coefficients[i];
+				xyz[1] += Y.m_Coefficients[i] * m_Coefficients[i];
+				xyz[2] += Z.m_Coefficients[i] * m_Coefficients[i];
+			}
+
+			real scale =
+				((real)s_SampledLambdaEnd - (real)s_SampledLambdaStart) / ((real)s_NumSpectralSamples * s_CIELuminanceFactor);
+			xyz[0] *= scale;
+			xyz[1] *= scale;
+			xyz[2] *= scale;
+
+			return xyz;
+		}
+
+		std::array<real, 3> ToRGB() const
+		{
+			std::array<real, 3> xyz = ToXYZ();
+			return XYZToRGB(xyz);
+		}
+
+		real CieY() const
+		{
+			real y = 0;
+			for (int i = 0; i < s_NumSpectralSamples; i++)
+			{
+				y += Y.m_Coefficients[i] * m_Coefficients[i];
+			}
+
+			real scale =
+				((real)s_SampledLambdaEnd - (real)s_SampledLambdaStart) / ((real)s_NumSpectralSamples * s_CIELuminanceFactor);
+			y *= scale;
+
+			return y;
 		}
 
 		static SampledSpectrum FromSamples(const real* lambda, const real* values, int n)
@@ -136,7 +202,7 @@ namespace yart
 			SampledSpectrum ret;
 			for (int i = 0; i < s_NumSpectralSamples; i++)
 			{
-				// calculate range of wavelengths the ith sample represents
+				// calculate range of wavelengths the ith sample covers
 				real lambdaStart =
 					Lerp((real)i / (real)s_NumSpectralSamples, (real)s_SampledLambdaStart, (real)s_SampledLambdaEnd);
 				real lambdaEnd =
@@ -148,6 +214,9 @@ namespace yart
 			return ret;
 		}
 
+		static void Initialize();
+
 	private:
+		static SampledSpectrum X, Y, Z;
 	};
 }
--- YART-Lib/src/core/yart.cpp
+++ YART-Lib/src/core/yart.cpp
@@ -1,10 +1,12 @@
 #include "pch.h"
 #include "core/yart.h"
+#include "core/spectrum.h"
 
 namespace yart
 {
 	void Initialize()
 	{
 		Log::Initialize();
+		SampledSpectrum::Initialize();
 	}
 }
